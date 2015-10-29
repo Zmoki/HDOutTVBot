56531cea8669200e4a5e5fc64abef3d04608e024
Zarema Khalilova <zarema.khalilova@gmail.com>
2015-10-29 23:53:17 +0300

    Add command /yestertoday

--- index.js
+++ index.js
@@ -16,6 +16,25 @@ const months = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', '
 let today = new Date();
 today = [today.getDate(), months[today.getMonth()], today.getFullYear()];
 today = today.join(' ');
+let yesterday = new Date(new Date().setDate(new Date().getDate() - 1));
+yesterday = [yesterday.getDate(), months[yesterday.getMonth()], yesterday.getFullYear()];
+yesterday = yesterday.join(' ');
+
+bot.onText(/\/start|help/, (msg)=>{
+  let chatId = msg.chat.id;
+  let response = '/today - новые эпизоды на сегодня\n'
+      + '/yesterday - новые эпизоды за вчера';
+  var opts = {
+    reply_to_message_id: msg.message_id,
+    reply_markup: JSON.stringify({
+      keyboard: [
+        ['/today'],
+        ['/yesterday']
+      ]
+    })
+  };
+  bot.sendMessage(chatId, response, opts);
+});
 
 bot.onText(/\/today/, (msg)=>{
   let chatId = msg.chat.id;
@@ -29,7 +48,33 @@ bot.onText(/\/today/, (msg)=>{
 
       if(newEpisodes.length){
         newEpisodes.forEach(episode=>{
-          response += '[' + episode.title + '](' + episode.link + ')\n\t';
+          response += episode.title + ' - ' + episode.link + '\n';
+        });
+      } else{
+        response = 'No new episodes';
+      }
+
+      bot.sendMessage(chatId, response);
+    });
+  });
+});
+
+/**
+ * TODO delete dublicate code, refactoring this
+ */
+bot.onText(/\/yesterday/, (msg)=>{
+  let chatId = msg.chat.id;
+  let response = '';
+
+  request('http://hdout.tv/RSS/').then(data=>{
+    parseString(data.body, (err, result)=>{
+      let newEpisodes = result.rss.channel[0].item.filter(item=>{
+        return item.pubDate[0].indexOf(yesterday) > -1;
+      });
+
+      if(newEpisodes.length){
+        newEpisodes.forEach(episode=>{
+          response += episode.title + ' - ' + episode.link + '\n';
         });
       } else{
         response = 'No new episodes';
