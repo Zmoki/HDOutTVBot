95eab319256df148d7c3c450fffbbfcfa79e8802
Zarema Khalilova <zarema.khalilova@gmail.com>
2015-10-29 22:59:19 +0300

    Send last 10 new episodes to bot

--- index.js
+++ index.js
@@ -3,10 +3,28 @@
 const Promise = require('bluebird');
 const request = Promise.promisify(require('request'));
 const parseString = require('xml2js').parseString;
+const TelegramBot = require('node-telegram-bot-api');
 
-request('http://hdout.tv/RSS/').then(data=>{
-  parseString(data.body, (err, result)=>{
-    let newEpisodes = result.rss.channel[0].item;
-    console.log(newEpisodes);
+const TelegramBotToken = process.env.TELEGRAM_BOT_TOKEN || '169257788:AAE_Nqcs762uKLlq3of2-mLZaS8doPcT5p0';
+const TelegramBotOptions = {
+  polling: true
+};
+
+let bot = new TelegramBot(TelegramBotToken, TelegramBotOptions);
+
+bot.onText(/\/new/, (msg)=>{
+  let chatId = msg.chat.id;
+  let response = '';
+
+  request('http://hdout.tv/RSS/').then(data=>{
+    parseString(data.body, (err, result)=>{
+      let newEpisodes = result.rss.channel[0].item;
+
+      for(let i = 0; i < 10; i++){
+        response += '[' + newEpisodes[i].title + '](' + newEpisodes[i].link + ')\n\t';
+      }
+
+      bot.sendMessage(chatId, response);
+    });
   });
 });
